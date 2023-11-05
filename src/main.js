ee648c16990895186da021c09766e1f5d7bfd0cf
mywristbands <41599300+mywristbands@users.noreply.github.com>
2023-11-04 18:21:29 -0700

    one more

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
+/*
 
+Ethan Heffan
+This Game Really Makes You Feel Like A Hamster
+30+ Hours
+
+Creative Tilt:
+I really challenged myself by formulating the entire Endless Runner as a spinning wheel with groups of objects.
+This required me to really understand arcade collisions, containers, inheritance, and hamster physics, which was
+very challenging to figure out on my own. I think it ended up being a fairly new endless runner type that I'm
+definitely proud of.
+
+I definitely am not an "artist" (however you wanna define that), but I finally caved and bought Aseprite to create
+the pixel art assets and learn that whole process. I think the theme is incredibly cute. Hamsters in a hamster wheel
+are the irl endless runner, so I wanted to translate that experience into the crisp format of beginner browser game.
+
+*/
 
 let config = {
     type: Phaser.CANVAS,
